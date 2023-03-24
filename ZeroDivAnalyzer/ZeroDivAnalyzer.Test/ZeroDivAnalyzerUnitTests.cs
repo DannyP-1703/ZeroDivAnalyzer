d54c9f42be844681a6b1f9dfbb8a6d49b38e13e2
mrhar <mrhar@DESKTOP-VR694LH>
2023-03-24 17:33:25 +0300

    Добавьте файлы проекта.

---
+++ ZeroDivAnalyzer/ZeroDivAnalyzer.Test/ZeroDivAnalyzerUnitTests.cs
@@ -0,0 +1,244 @@
+﻿using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = ZeroDivAnalyzer.Test.CSharpAnalyzerVerifier<
+    ZeroDivAnalyzer.ZeroDivAnalyzerAnalyzer>;
+
+
+
+namespace ZeroDivAnalyzer.Test
+{
+    [TestClass]
+    public class ZeroDivAnalyzerUnitTest
+    {
+        [TestMethod]
+        public async Task TestNoDiv_NoDiagnostics()
+        {
+            var test = @"
+namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            int a = 6;
+        }
+    }";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TestSimpleLiteral()
+        {
+            var test = @"
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            int a = {|CS0020:(35 + 89) / 0|};
+        }
+    }";
+            var expected = VerifyCS.Diagnostic("ZeroDivAnalyzer").WithSpan(6, 21, 6, 34).WithArguments("0");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task TestVarEqualsZero()
+        {
+            var test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            int b = 1;
+
+            private static void Main(string[] args)
+            {
+                int a = 5;
+                int b = 0;
+                Console.WriteLine(a + [|a / b|]);
+            }
+        }
+    }";
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TestVarReassigned_NoDiagnostics()
+        {
+            var test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            int b = 1;
+
+            private static int foo() { return 5; }
+
+            private static void Main(string[] args)
+            {
+                int b = 0;
+                int a = 5;
+                b = foo();
+                Console.WriteLine(a + a / b);
+            }
+        }
+    }";
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TestSubtractionLiteral()
+        {
+            var test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            private static void Main(string[] args)
+            {
+                Console.WriteLine({|CS0020:(8745 - 855) / (5 - 5)|});
+            }
+        }
+    }";
+            var expected = VerifyCS.Diagnostic("ZeroDivAnalyzer").WithSpan(10, 35, 10, 57).WithArguments("5 - 5");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [TestMethod]
+        public async Task TestSubtractionVar()
+        {
+            var test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            private static int foo() { return 5; }
+            private static void Main(string[] args)
+            {
+                Int64 val = 5;
+                Console.WriteLine([|foo() / (val - val)|]);
+            }
+        }
+    }";
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TestSubtraction1_NoDiagnostics()
+        {
+            var test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            private static int foo() { return 7; }
+            private static void Main(string[] args)
+            {
+                Console.WriteLine((8745 - 855) / (foo() - foo()));
+            }
+        }
+    }";
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TestSubtraction2_NoDiagnostics()
+        {
+            var test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            private static void Main(string[] args)
+            {
+                int a = 6;
+                int b = 5;
+                Console.WriteLine(b / (a - a + 1));
+            }
+        }
+    }";
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TestMultiplyVar()
+        {
+            var test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            private static int foo() { return 5; }
+
+            private static void Main(string[] args)
+            {
+                int b = 0;
+                int a = 0;
+                b = foo();
+                Console.WriteLine(a + [|a / (b * a)|]);
+            }
+        }
+    }";
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TestMultiplyLiteral()
+        {
+            var test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            private static int foo() { return 5; }
+
+            private static void Main(string[] args)
+            {
+                int a = 5;
+                Console.WriteLine(a + [|a / (0 * a)|]);
+            }
+        }
+    }";
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task TestMultiply_NoDiagnostics()
+        {
+            var test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class Program
+        {   
+            private static int foo() { return 5; }
+
+            private static void Main(string[] args)
+            {
+                int a = 5, b = 0;
+                b = a + b;
+                Console.WriteLine(a + a / (b * a));
+            }
+        }
+    }";
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+    }
+}
---
+++ ZeroDivAnalyzer/ZeroDivAnalyzer/ZeroDivAnalyzerAnalyzer.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+
+
+namespace ZeroDivAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ZeroDivAnalyzerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "ZeroDivAnalyzer";
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Using";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.DivideExpression);
+        }
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            BinaryExpressionSyntax divideExpression = context.Node as BinaryExpressionSyntax;
+            ExpressionSyntax divisorNode = divideExpression.Right;
+            if (divisorNode.Kind() == SyntaxKind.ParenthesizedExpression)
+            {
+                divisorNode = (ExpressionSyntax)divisorNode.ChildNodes().First();
+            }
+
+            var diagnostic = Diagnostic.Create(Rule, context.Node.GetLocation(), divisorNode.ToString());
+
+            switch (divisorNode.Kind())
+            {
+                case SyntaxKind.NumericLiteralExpression:
+                    if (IsLiteralZero(divisorNode))
+                    {
+                        context.ReportDiagnostic(diagnostic);
+                        Console.WriteLine(diagnostic.ToString());
+                    }
+                    break;
+
+                case SyntaxKind.IdentifierName:
+                    if (IsVarZero(context, divisorNode))
+                    {
+                        context.ReportDiagnostic(diagnostic);
+                        Console.WriteLine(diagnostic.ToString());
+                    }
+                    break;
+
+                case SyntaxKind.SubtractExpression:
+                    var leftNode = ((BinaryExpressionSyntax)divisorNode).Left;
+                    var rightNode = ((BinaryExpressionSyntax)divisorNode).Right;
+                    bool error = false;
+                    if (leftNode.Kind() == SyntaxKind.NumericLiteralExpression)
+                    {
+                        error = leftNode.ToString().Equals(rightNode.ToString());
+                    }
+                    if (leftNode.Kind() == SyntaxKind.IdentifierName)
+                    {
+                        var leftSymbol = context.SemanticModel.GetSymbolInfo(leftNode).Symbol;
+                        var rightSymbol = context.SemanticModel.GetSymbolInfo(rightNode).Symbol;
+                        if (leftSymbol.Kind == SymbolKind.Local || leftSymbol.Kind == SymbolKind.Field)
+                        {
+                            error = leftSymbol.Equals(rightSymbol, SymbolEqualityComparer.Default);
+                        }
+                    }
+
+                    if (error)
+                    {
+                        context.ReportDiagnostic(diagnostic);
+                        Console.WriteLine(diagnostic.ToString());
+                    }
+                    break;
+
+                case SyntaxKind.MultiplyExpression:
+                    leftNode = ((BinaryExpressionSyntax)divisorNode).Left;
+                    rightNode = ((BinaryExpressionSyntax)divisorNode).Right;
+                    error = leftNode.Kind() == SyntaxKind.NumericLiteralExpression && IsLiteralZero(leftNode)
+                            || rightNode.Kind() == SyntaxKind.NumericLiteralExpression && IsLiteralZero(rightNode)
+                            || leftNode.Kind() == SyntaxKind.IdentifierName && IsVarZero(context, leftNode)
+                            || rightNode.Kind() == SyntaxKind.IdentifierName && IsVarZero(context, rightNode);
+
+                    if (error)
+                    {
+                        context.ReportDiagnostic(diagnostic);
+                        Console.WriteLine(diagnostic.ToString());
+                    }
+                    break;
+            }
+        }
+
+        private static bool IsLiteralZero(ExpressionSyntax divisor)
+        {
+            SyntaxToken token = divisor.ChildTokens().First();
+            return token.Kind() == SyntaxKind.NumericLiteralToken && token.Value.ToString() == "0";
+        }
+
+        private static bool IsVarZero(SyntaxNodeAnalysisContext context, ExpressionSyntax divisor)
+        {
+            ISymbol variable = context.SemanticModel.GetSymbolInfo(divisor).Symbol;
+            if (variable.DeclaringSyntaxReferences != null && variable.Kind == SymbolKind.Local)
+            {
+                VariableDeclaratorSyntax declarationNode = variable.DeclaringSyntaxReferences.First().GetSyntax() as VariableDeclaratorSyntax;
+
+                SyntaxNode declarationParent = declarationNode.GetLastToken().GetNextToken().GetNextToken().Parent;
+                SyntaxNode divisorParent = divisor;
+                while (!(declarationParent is StatementSyntax))
+                {
+                    declarationParent = declarationParent.Parent;
+                }
+                while (!(divisorParent is StatementSyntax))
+                {
+                    divisorParent = divisorParent.Parent;
+                }
+
+                try
+                {
+                    DataFlowAnalysis dataFlowAnalysis = context.SemanticModel.AnalyzeDataFlow(declarationParent, divisorParent);
+
+                    var declaratorExpressionSyntax = declarationNode.Initializer.Value;
+                    if (declaratorExpressionSyntax.Kind() == SyntaxKind.NumericLiteralExpression
+                        && Int32.Parse(declaratorExpressionSyntax.ChildTokens().First().ValueText) == 0
+                        && !dataFlowAnalysis.WrittenInside.Contains(variable))
+                    {
+                        return true;
+                    }
+                }
+                catch (ArgumentException) { return false; }
+            }
+            return false;
+        }
+    }
+}
